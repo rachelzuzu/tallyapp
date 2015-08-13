86e4386b38b71be9c9e6b2266aa2ba24fe365f58
Rachel Zuzu <rachelzuzu4@gmail.com>
2015-08-13 05:30:05 -0700

    update comments

--- public/core.js
+++ public/core.js
@@ -1,8 +1,11 @@
+// angular code
+
 var tallyApp = angular.module('tallyApp', []);
 
 function MainCtrl($scope, $http) {
 	$scope.formData = {};
 
+	// when landing on the page, get all posts and show them
 	$http.get('/api/posts')
 		.success(function(data) {
 			$scope.posts = data;
@@ -12,6 +15,7 @@ function MainCtrl($scope, $http) {
 			console.log('Error: ' + data);
 		});
 
+	// when submitting, send the text to the node API
 	$scope.createPost = function() {
 		$http.post('/api/posts', $scope.formData)
 			.success(function(data) {
@@ -23,7 +27,8 @@ function MainCtrl($scope, $http) {
 				console.log('Error: ' + data);
 			});
 	};
-	//delete post
+
+	//delete post after checking it
 	$scope.deletePost = function(id) {
 		$http.delete('/api/posts/' + id)
 			.success(function(data) {
@@ -34,6 +39,8 @@ function MainCtrl($scope, $http) {
 				console.log('Error: ' +data);
 			});
 	};
+
+	//calculate total of posts
 	$scope.total = function() {
 		var total = 0;
 		angular.forEach($scope.posts, function(post) {
--- server.js
+++ server.js
@@ -1,21 +1,32 @@
+// node configuration
+
+// set up
 var express = require('express');
+// create our app w/ express
 var app = express();
+// mongoose for mongodb
 var mongoose = require('mongoose');
+// log requests to the console (express4)
 var morgan = require('morgan');
+// pull information from HTML POST (express4)
 var bodyParser = require('body-parser');
+// simulate DELETE and PUT (express4)
 var methodOverride = require('method-override');
-var moment = require('moment');
+var port = process.env.PORT || 8000;
 
+// use mongoose.connect to connect to modulus.io remote db
 mongoose.connect('mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu');
-
+// configuration w express modules
 app.use(express.static(__dirname + '/public'));
+// log request to console
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({'extended':'true'}));
+// parse app/json
 app.use(bodyParser.json());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 app.use(methodOverride());
 
-// create post model
+// create mongoose model; monogo automatically generates _id
 var Post = mongoose.model('Post', {
 	text : String,
 	amount: Number,
@@ -23,8 +34,8 @@ var Post = mongoose.model('Post', {
 	total: Number
 });
 
-
-// routes
+// RESTful api routes with Express routes to handle api calls
+// On page load, GET /api/todos and bind the JSON received from API to $scope.posts. they are looped over in view
 app.get('/api/posts', function(req, res) {
 
 	// use mongoose to  get all posts in db
@@ -40,6 +51,7 @@ app.get('/api/posts', function(req, res) {
 // create post and send back all posts after creation
 app.post('/api/posts', function(req, res) {
 	
+	// create a post, information comes from AJAX request from Angular
 	Post.create({
 		text : req.body.text,
 		amount: req.body.amount,
@@ -49,7 +61,7 @@ app.post('/api/posts', function(req, res) {
 	}, function(err, post) {
 		if (err)
 			res.send(err);
-
+		// get and return all posts after you create another
 		Post.find(function(err, posts) {
 			if (err)
 				res.send(err)
@@ -64,7 +76,7 @@ app.delete('/api/posts/:post_id', function(req, res) {
 	}, function(err, post) {
 		if (err)
 			res.send(err);
-
+		// get and return all posts after you create another
 		Post.find(function(err, posts) {
 			if (err)
 				res.send(err)
@@ -76,11 +88,11 @@ app.delete('/api/posts/:post_id', function(req, res) {
 
 // add route for front end angular
 app.get('*', function(req, res) {
-	res.sendFile( '/public/index.html');
+	res.sendFile( './public/index.html');
 });
 
 
 
 
 // listen (start app with node server.js) ======================================
-app.listen(8000);
+app.listen(port);
